5dcb8b52bbd27f765b2d42626636cfda0e158022
André Voronhuk <andrevoronhuk@gmail.com>
2022-07-14 14:57:30 -0300

    estilização da pagina de cadastro de pedidos

--- src/main/java/br/com/alura/mvc/mudi/DAO/PedidoDAO.java
+++ src/main/java/br/com/alura/mvc/mudi/DAO/PedidoDAO.java
@@ -1,9 +1,6 @@
 package br.com.alura.mvc.mudi.DAO;
 
-import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,17 +1,11 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.mudi.DAO.PedidoDAO;
-import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
@@ -25,5 +19,6 @@ public class HomeController {
                 mv.addObject("pedidos", pedidoDAO.findAll());
                 return mv;
         }
+        
 
 }
